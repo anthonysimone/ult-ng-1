aa0c6b39ec3cc19be8faf7e959a3e892227a5d1c
Anthony Simone <anthony.simone7@gmail.com>
2019-08-11 08:49:04 -0700

    Finish forms

---
+++ app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
@@ -0,0 +1,113 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+import { Passenger } from '../../models/passenger.interface';
+import { Baggage } from '../../models/baggage.interface';
+
+@Component({
+  selector: 'passenger-form',
+  styleUrls: ['passenger-form.component.scss'],
+  template: `
+    <form #form="ngForm" (ngSubmit)="handleSubmit(form.value, form.valid)">
+
+      <div>
+        Passenger name:
+        <input
+          type="text"
+          name="fullname"
+          required
+          #fullname="ngModel"
+          [ngModel]="detail?.fullname">
+        <div class="error"
+          *ngIf="fullname.errors?.required && fullname.dirty">
+          Passenger name is required
+        </div>
+      </div>
+
+      <div>
+        Passenger ID:
+        <input
+          type="number"
+          name="id"
+          required
+          #id="ngModel"
+          [ngModel]="detail?.id">
+        <div class="error"
+          *ngIf="id.errors?.required && id.dirty">
+          Passenger ID is required
+        </div>
+      </div>
+
+      <div>
+        <label>
+          <input
+            type="checkbox"
+            name="checkedIn"
+            [ngModel]="detail?.checkedIn"
+            (ngModelChange)="toggleCheckIn($event)">
+          Yes
+        </label>
+      </div>
+
+      <div *ngIf="form.value.checkedIn">
+        Check in date:
+        <input
+          type="number"
+          name="checkInDate"
+          [ngModel]="detail?.checkInDate">
+      </div>
+
+      <div>
+        <label>Baggage</label>
+        <select
+          name="baggage"
+          [ngModel]="detail?.baggage">
+          <option
+            *ngFor="let item of baggage"
+            [value]="item.key"
+            [selected]="item.key === detail?.baggage">
+            {{ item.value }}
+          </option>
+        </select>
+      </div>
+
+      <button type="submit" [disabled]="form.invalid">
+        Update passenger
+      </button>
+    </form>
+  `
+})
+export class PassengerFormComponent {
+  @Input()
+  detail: Passenger;
+
+  @Output()
+  update: EventEmitter<any> = new EventEmitter();
+
+  baggage: Baggage[] = [{
+    key: 'none',
+    value: 'No baggage'
+  },{
+    key: 'hand-only',
+    value: 'Hand baggage'
+  },{
+    key: 'hold-only',
+    value: 'Hold baggage'
+  },{
+    key: 'hand-hold',
+    value: 'Hand and hold baggage'
+  }];
+
+  toggleCheckIn(checkedIn: boolean) {
+    if (checkedIn) {
+      this.detail.checkInDate = Date.now();
+    }
+  }
+
+  handleSubmit(passenger: Passenger, isValid: boolean) {
+    if (isValid) {
+      console.log('hey');
+      // this.udpate.emit();
+      this.update.emit(passenger);
+    }
+  }
+}
